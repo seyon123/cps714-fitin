d86c1a2a730c6222de4f5b7984da6ead268f0c6a
Raj Mistry <56315897+raj-mistry@users.noreply.github.com>
2022-10-25 22:29:19 -0400

    adding tag component
    add tag component and file upload

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,27 +1,41 @@
-import React from "react";
+
+import React, { useState } from "react";
 //import { Row, Col, Button, Container } from "react-bootstrap";
 import "./CreatePost.css";
 //import Card from "react-bootstrap/Card";
+import { TagsInput } from "react-tag-input-component";
 
 function CreatePost() {
 
+    const [selected, setSelected] = useState(["papaya"]);
+    function uploadContent(e) {
+        e.preventDefault();
+        document.getElementById("imageUpload").click()
+      }
+
 	return (
-		<div class="postBackground">
-            <div class="formStyle">
-            <div class="form-group">
-                <label for="postTextArea" class="labelStyle">Post</label>
-                <textarea class="form-control" rows="6" id="postTextArea">hook this up to a text variable</textarea>
+		<div className="postBackground">
+            <div className="formStyle">
+                <div className="form-group">
+                    <label for="postTextArea" className="labelStyle">Post</label>
+                    <textarea class="form-control inputArea" rows="6" id="postTextArea">hook this up to a text variable</textarea>
+                </div>
+                <div className="form-group" style={{marginTop:10}}>
+                <label for="tagsInput" class="labelStyle">Tags</label>
+                <TagsInput
+                    id="tagsInput"
+                    className="form-control"
+                    value={selected}
+                    onChange={setSelected}
+                    name="fruits"
+                />
+                </div>
             </div>
-            <div class="form-group" style={{marginTop:10}}>
-            <label for="tagsInput" class="labelStyle">Tags</label>
-            <input type="text" class="form-control" id="tagsInput" placeholder="Enter tags" ></input>
-            </div>
-
-
-            </div>
-            <div class="input-group mb-3 uploadBar formStyle">
-            <input type="file" class="form-control" id="inputGroupFile02" ></input>
-            <button class="postButtonStyle">Post</button>
+            <div className="input-group mb-3 uploadBar formStyle">
+            <img src="camera.png" style={{display:"block", height:"30px",width:"auto",marginRight:"10px"}} onClick={uploadContent}/>
+            <img src="pictures.png" style={{display:"block", height:"30px",width:"auto"}} onClick={uploadContent}/>
+            <input type="file" style={{visibility: "hidden"}} className="iconFormat" id="imageUpload" ></input>
+            <button className="postButtonStyle">Post</button>
             </div>
         </div>
 	);
