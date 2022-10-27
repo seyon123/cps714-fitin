9cb7325adacd536f21d8ffbc6946293325e50e7f
Hamdan <khanhamdan16200@gmail.com>
2022-10-27 12:40:36 -0400

    SPM-67 created friends list component

---
+++ src/components/FriendsList.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Container, Row, Col} from "react-bootstrap";
+
+import "./FriendsList.css";
+
+function FriendsList(){
+    const dummyFriends = [
+        {id: 0, name: "Jacky", image: "usericon1.png"},
+        {id: 1, name: "Seyon", image: "usericon1.png"},
+        {id: 2, name: "William", image: "usericon1.png"},
+        {id: 3, name: "Tiago", image: "usericon1.png"},
+        {id: 4, name: "Raj", image: "usericon1.png"},
+        {id: 5, name: "Hamdan", image: "usericon1.png"},
+        {id: 5, name: "Noah", image: "usericon1.png"},
+        {id: 5, name: "Jawwad", image: "usericon1.png"},
+        {id: 5, name: "Leslie", image: "usericon1.png"},
+        {id: 5, name: "Haaland", image: "usericon1.png"},
+        {id: 5, name: "Ronaldo", image: "usericon1.png"},
+        {id: 5, name: "Messi", image: "usericon1.png"},
+        {id: 5, name: "Tobi", image: "usericon1.png"},
+        {id: 5, name: "Rowlet", image: "usericon1.png"}
+    ];
+    
+    const [friends, setFriends] = useState(dummyFriends);
+
+    return (
+        <Container fluid className="friends-list-wrap px-0">
+            <div className="friends-list-header rounded-top">
+                <h3 className="px-3">Friends</h3>
+            </div>
+
+            <div className="friends-list-body px-4 pt-2">
+            
+            {friends.map(({ id, image, name })=>(
+                <Row className="mb-1" onClick={(event)=>{alert("Clicked on " + id + " " + name)}}>
+                    <Col md="3" className="p-1"><img src={image} className="w-100 rounded-circle" alt="" /></Col>
+                    <Col md="auto" className="align-self-center"><p className="friend-name">{name}</p></Col>   
+                </Row>
+            ))}
+            </div>
+        </Container>
+    );
+
+}
+
+export default FriendsList;
