0fc2e667df1bdb8a636ecc176bc0c1ce9a6ba353
Seyon Rajagopal <r_seyon@live.ca>
2022-11-01 12:29:02 -0400

    fix styling of friends component

--- src/pages/SocialPage.js
+++ src/pages/SocialPage.js
@@ -27,15 +27,15 @@ function SocialPage() {
 	return (
 		<Container fluid className="mainPage p-0 m-0">
 			<div className="row">
-				<div className="p-0 m-0 pe-5 mt-2 col-md-3">
+				<div className="p-0 m-0 pe-5 col-md-3">
 					<FriendsList />
 				</div>
 
 				<div className="p-0 m-0 col-md-8">
 					<Col>
-					<div style={{margin: "5px"}}>
-					<CreatePost></CreatePost>
-					</div>
+						<div style={{ margin: "5px" }}>
+							<CreatePost></CreatePost>
+						</div>
 						<Row className="noMargin">Name: {user.displayName}</Row>
 						<Row className="noMargin">Email: {user.email}</Row>
 						<Row className="noMargin">ID: {user.uid}</Row>
