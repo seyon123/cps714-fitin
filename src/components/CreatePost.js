63e9596dadd3f62a759b9219459504cdf0b4d1b1
Seyon Rajagopal <r_seyon@live.ca>
2022-11-03 01:17:38 -0400

    fix uploading blank image

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -3,7 +3,7 @@ import "./CreatePost.css";
 import { TagsInput } from "react-tag-input-component";
 import { useAuth } from "../contexts/AuthContext";
 import { storage, db } from "../firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { AiFillCamera, AiFillPicture, AiFillCloseCircle } from "react-icons/ai";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
@@ -13,7 +13,7 @@ function CreatePost() {
 	const [tags, setTags] = useState([]);
 	const [postText, setPostText] = useState("");
 	const [file, setFile] = useState(null);
-	const [fileName, setFileName] = useState("");
+	const [fileName, setFileName] = useState(null);
 
 	function uploadContent() {
 		document.getElementById("imageUpload").click();
@@ -28,7 +28,7 @@ function CreatePost() {
 	};
 
 	const handleTrash = () => {
-		setFileName("");
+		setFileName(null);
 		setFile(null);
 	};
 
@@ -36,64 +36,74 @@ function CreatePost() {
 		setTags([]);
 		setPostText("");
 		setFile(null);
-		setFileName("");
+		setFileName(null);
 	}
 
-	function postContent() {
+	async function postContent() {
 		// Upload file and metadata to the object 'images/mountains.jpg'
 		const storageRef = ref(storage, `images/${currentUser?.uid}/${Math.floor(Math.random() * (9999 - 1000)) + 1000}-${fileName}`);
-		const uploadTask = uploadBytesResumable(storageRef, file);
+		const uploadTask = file && uploadBytesResumable(storageRef, file);
 
 		// Listen for state changes, errors, and completion of the upload.
-		uploadTask.on(
-			"state_changed",
-			(snapshot) => {
-				// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				console.log("Upload is " + progress + "% done");
-				switch (snapshot.state) {
-					case "paused":
-						console.log("Upload is paused");
-						break;
-					case "running":
-						console.log("Upload is running");
-						break;
-					default:
-						break;
-				}
-			},
-			(error) => {
-				// A full list of error codes is available at
-				// https://firebase.google.com/docs/storage/web/handle-errors
-				switch (error.code) {
-					case "storage/unauthorized":
-						// User doesn't have permission to access the object
-						break;
-					case "storage/canceled":
-						// User canceled the upload
-						break;
-					// ...
-					case "storage/unknown":
-						// Unknown error occurred, inspect error.serverResponse
-						break;
-					default:
-						break;
-				}
-			},
-			() => {
-				// Upload completed successfully, now we can get the download URL
-				getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-					console.log("File available at", downloadURL);
-					await addDoc(collection(db, `posts`), {
-						uid: currentUser.uid,
-						tags: tags,
-						description: postText,
-						image: downloadURL,
+		file &&
+			uploadTask.on(
+				"state_changed",
+				(snapshot) => {
+					// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log("Upload is " + progress + "% done");
+					switch (snapshot.state) {
+						case "paused":
+							console.log("Upload is paused");
+							break;
+						case "running":
+							console.log("Upload is running");
+							break;
+						default:
+							break;
+					}
+				},
+				(error) => {
+					// A full list of error codes is available at
+					// https://firebase.google.com/docs/storage/web/handle-errors
+					switch (error.code) {
+						case "storage/unauthorized":
+							// User doesn't have permission to access the object
+							break;
+						case "storage/canceled":
+							// User canceled the upload
+							break;
+						// ...
+						case "storage/unknown":
+							// Unknown error occurred, inspect error.serverResponse
+							break;
+						default:
+							break;
+					}
+				},
+				() => {
+					// Upload completed successfully, now we can get the download URL
+					getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+						console.log("File available at", downloadURL);
+						await addDoc(collection(db, `posts`), {
+							uid: currentUser.uid,
+							tags: tags,
+							description: postText,
+							timestamp: serverTimestamp(),
+							image: downloadURL,
+						});
 					});
-				});
-				resetCreatePost();
-			}
-		);
+				}
+			);
+		!file &&
+			postText &&
+			(await addDoc(collection(db, `posts`), {
+				uid: currentUser.uid,
+				tags: tags,
+				description: postText,
+				timestamp: serverTimestamp(),
+			}));
+		resetCreatePost();
 	}
 	return (
 		<div className="postBackground pb-1 mt-3">
