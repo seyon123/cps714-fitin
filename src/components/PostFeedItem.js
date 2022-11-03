c8825dcb0339c2cc7236dbfbd691622541bc0652
Seyon Rajagopal <r_seyon@live.ca>
2022-11-03 09:50:43 -0400

    read show posts from database

--- src/components/PostFeedItem.js
+++ src/components/PostFeedItem.js
@@ -1,36 +1,53 @@
 import "./PostFeedItem.css";
+import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
-import { FaTag } from 'react-icons/fa'
+import { FaTag } from "react-icons/fa";
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
+function PostFeedItem({ uid, tags, image, description }) {
+	const [user, setUser] = useState(null);
 
-function PostFeedItem({author, authorImage, tags, image, content}) {
-	
+	useEffect(() => {
+		//Get User from database
+		async function findUser() {
+			const docRef = doc(db, `users`, uid);
+			const docUserSnap = await getDoc(docRef);
+
+			if (docUserSnap.exists()) {
+				setUser({ ...docUserSnap.data(), id: docUserSnap.id });
+			}
+		}
+		uid && findUser();
+	}, [uid]);
+
 	return (
-		<Card bg='dark' text='white' className="postFeedItem">
+		<Card bg="dark" text="white" className="postFeedItem">
+			{console.log(user)}
 			<Card.Body>
-				<div className="postItemInline">
-					<img src={authorImage} className="postItemProfileImg rounded-circle postItemMarginRightSmall" alt="profile pic"/>
+				<div className="d-flex align-items-center justify-content-start">
+					<img src={user?.photoURL} className="postItemProfileImg rounded-circle me-1" alt={user?.name} />
 					<div>
-						<Card.Title>{author}</Card.Title>
-						<Card.Subtitle className="mb-2 text-muted">
-							{tags?.length > 0 &&
-								tags.map(({ tag, url }, id) => (
-									<div className="postItemInline postItemMarginRightSmall" key={id}>
+						<Card.Title>{user?.name}</Card.Title>
+						{tags?.length > 0 && (
+							<Card.Subtitle className="mb-2 text-muted d-flex align-items-center justify-content-start">
+								{tags.map((tag, id) => (
+									<div className="ms-1" key={id}>
 										<FaTag />
-										<Card.Link className="postTagLink" href={url}>{tag}</Card.Link>
+										<Card.Link className="postTagLink ms-1" href={tag}>
+											{tag}
+										</Card.Link>
 									</div>
-								))
-							}
-						</Card.Subtitle>
+								))}
+							</Card.Subtitle>
+						)}
 					</div>
 				</div>
 				<div>
-					{ (image) && <img src={image} className="postImage postItemMarginTopSmall" alt="profile pic" /> }
-					<div className="postItemMarginRightSmall postItemMarginTopSmall">
-						<Card.Text>
-							{content}
-						</Card.Text>
-					</div>	
+					{image && <img src={image} className="postImage rounded mt-1" alt={description} />}
+					<div className="postItemMarginRightSmall mt-1">
+						<Card.Text>{description}</Card.Text>
+					</div>
 				</div>
 			</Card.Body>
 		</Card>
--- src/pages/SocialPage.js
+++ src/pages/SocialPage.js
@@ -1,49 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import "./SocialPage.css";
 import PostFeedItem from "../components/PostFeedItem";
 import CreatePost from "../components/CreatePost";
-
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { db } from "../firebase";
 import FriendsList from "../components/FriendsList";
 
 function SocialPage() {
+	const [posts, setPosts] = useState([]);
+
 	useEffect(() => {
 		document.title = `Social Page | FitIn`;
 	}, []);
 
-	const posts = [
-		{
-			author: "Noah Colaco",
-			authorImage: "usericon1.png",
-			tags: [
-				{ tag: "Yoga", url: "#" },
-				{ tag: "Cardio", url: "#" },
-			],
-			image: "usericon1.png",
-			content: "this is the post content an example of typing this thing is making me type this idk bruh, but ya boi out here doing all this typing stuff and we built diff out here gang gang",
-		},
-		{
-			author: "Fabrizio Romano",
-			authorImage: "usericon1.png",
-			tags: [
-				{ tag: "Sports", url: "#" },
-				{ tag: "France", url: "#" },
-			],
-			image: "usericon1.png",
-			content: "Paul Pogba will miss the Qatar 2022 World Cup due to new injury, confirmed by his agent Rafaela Pimenta",
-		},
-		{
-			author: "Simon Collings",
-			authorImage: "usericon1.png",
-			tags: [
-				{ tag: "Sports", url: "#" },
-				{ tag: "Arsenal", url: "#" },
-			],
-			image: "",
-			content: "No training for squad today, but Arsenal due to assess Bukayo Saka. Hope is nothing serious, but club should know more by Wednesday.",
-		},
-	];
+	useEffect(() => {
+		onSnapshot(query(collection(db, `posts`), orderBy("timestamp", "desc")), (snapshot) => {
+			setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		});
+	}, []);
 
 	return (
 		<Container fluid className="mainPage">
@@ -57,10 +33,7 @@ function SocialPage() {
 					<Col>
 						<CreatePost></CreatePost>
 
-						{posts?.length > 0 &&
-							posts.map(({ author, authorImage, tags, image, content }, id) => (
-								<PostFeedItem author={author} authorImage={authorImage} tags={tags} image={image} content={content} key={id} />
-							))}
+						{posts?.length > 0 && posts.map(({ uid, tags, image, description }, id) => <PostFeedItem uid={uid} tags={tags} image={image} description={description} key={id} />)}
 					</Col>
 				</div>
 				<div className="p-0 m-0 col-md-2"></div>
