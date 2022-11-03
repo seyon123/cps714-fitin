654a84ba7cfff763e41dfacc2f16feead192254f
Seyon Rajagopal <r_seyon@live.ca>
2022-11-03 17:52:10 -0400

    add ability to add likes

--- src/components/PostFeedItem.js
+++ src/components/PostFeedItem.js
@@ -2,10 +2,19 @@ import "./PostFeedItem.css";
 import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { FaTag } from "react-icons/fa";
-import { getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { arrayUnion, arrayRemove, doc, getDoc, updateDoc } from "firebase/firestore";
+import { useAuth } from "../contexts/AuthContext";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-function PostFeedItem({ userRef, tags, image, description }) {
+import Moment from "react-moment";
+import "moment";
+import "moment-timezone";
+
+function PostFeedItem({ id, userRef, timestamp, tags, image, description, likes, comments }) {
+	const { currentUser } = useAuth();
 	const [user, setUser] = useState(null);
+	const [like, setLike] = useState(false);
 
 	useEffect(() => {
 		//Get User from database
@@ -17,6 +26,31 @@ function PostFeedItem({ userRef, tags, image, description }) {
 		}
 		userRef && findUser();
 	}, [userRef]);
+
+	useEffect(() => {
+		//set like to true if in likes
+		if (likes) {
+			console.log(likes.includes(currentUser.uid));
+			setLike(likes.includes(currentUser.uid));
+		}
+	}, [likes, currentUser.uid]);
+
+	async function likePost() {
+		const postRef = doc(db, "posts", id);
+		await updateDoc(postRef, { likes: arrayUnion(currentUser.uid) });
+		setLike(true);
+	}
+	async function unlikePost() {
+		const postRef = doc(db, "posts", id);
+		await updateDoc(postRef, { likes: arrayRemove(currentUser.uid) });
+		setLike(false);
+	}
+
+	function checkDateBeforeYesterday(date) {
+		const yesterday = new Date();
+		yesterday.setDate(yesterday.getDate() - 2);
+		return date < yesterday;
+	}
 
 	return (
 		<Card bg="dark" text="white" className="postFeedItem">
@@ -44,6 +78,13 @@ function PostFeedItem({ userRef, tags, image, description }) {
 					<div className="postItemMarginRightSmall mt-1">
 						<Card.Text>{description}</Card.Text>
 					</div>
+					<div className="mt-2 postTime">
+						{checkDateBeforeYesterday(timestamp.toDate()) ? <Moment format="LL">{timestamp.toDate()}</Moment> : <Moment fromNow>{timestamp.toDate()}</Moment>}
+					</div>
+				</div>
+				<div className="mt-2">
+					{like ? <FaHeart color="red" size="20px" role="button" onClick={() => unlikePost()} /> : <FaRegHeart size="20px" role="button" onClick={() => likePost()} />}{" "}
+					{likes?.length > 0 && likes.length}
 				</div>
 			</Card.Body>
 		</Card>
