37882e2fb23197ba2ca3be68fed75a8f5b393634
Jawwad Khan <jawwad1809@gmail.com>
2022-11-03 21:36:53 -0400

    added stuff to help page

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,26 +1,35 @@
-import React,{ useEffect } from "react";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { NavLink } from "react-router-dom";
 
 import "./HelpPage.css";
-
+import { Card } from "react-bootstrap";
 
 function HelpPage() {
-	// Set the page title
-	useEffect(() => {
-		document.title = `Help | FitIn`;
-	}, []);
-    return (
-        <Container fluid className="mainPage px-4">
-			<h1 className="pt-4">Help!</h1>
-			<hr></hr>
-            
-            </Container>
-
-
-    );
-
+  // Set the page title
+  useEffect(() => {
+    document.title = `Help | FitIn`;
+  }, []);
+  return (
+    <Container fluid className="mainPage px-4">
+      <Card bg="dark" className="helpbox">
+        <Card.Body>
+          <h1 className="pt-4">Help!</h1>
+          <hr></hr>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 export default HelpPage;
