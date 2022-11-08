b6e5a6f4afb54445354f3b88413da9f7a332cd9e
Leslie <leslie.wen@gmail.com>
2022-11-08 18:34:34 -0500

    SearchBar Frontend functional with dummy data

--- src/components/SocialSearchBar.js
+++ src/components/SocialSearchBar.js
@@ -1,11 +1,47 @@
 import React from "react";
 import "./SocialSearchBar.css";
-function SocialSearchBar(){
+import {useState} from 'react';
+function SocialSearchBar(){    
     
-    
+    const [searchTerm,setSearchTerm]=useState("")
+    const dummyData= [{
+            "id":"MyPost",
+            "userRef":2,
+            "timestamp":3,
+            "tags":3,
+            "image":3,
+            "description":3,
+            "likes":3
+        },{
+            "id":"YourPost",
+            "userRef":2,
+            "timestamp":3,
+            "tags":3,
+            "image":3,
+            "description":3,
+            "likes":3
+        }]
+
     return(
     <div className="SearchBar">
-        <input type="text" placeholder="Search..."/>   
-    </div>);
+        <input className="form-control" type="text" placeholder=" Search for a post..." onChange={(event)=>{
+            setSearchTerm(event.target.value);
+        }}/>   
+        
+        {dummyData.filter((val)=>{
+            if(searchTerm===""){
+                return ""
+            }else if(val.id.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val
+            }
+        }).map((val,key)=>{
+            return(
+                <div className="ResultsItem">
+                <p>{val.id}</p>
+                </div>
+            );
+        })}
+    </div>
+    );
 }
 export default SocialSearchBar;
