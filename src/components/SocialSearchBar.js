3dffe27975aeaef5068d63df79c82c9e529cd546
Seyon Rajagopal <r_seyon@live.ca>
2022-11-09 00:57:33 -0500

    read posts from backend and SPM-119 bugs

--- src/components/SocialSearchBar.js
+++ src/components/SocialSearchBar.js
@@ -1,50 +1,47 @@
 import React from "react";
 import "./SocialSearchBar.css";
-import {useState} from 'react';
-function SocialSearchBar(){    
-    
-    const [searchTerm,setSearchTerm]=useState("")
-    const dummyData= [{
-            "id":"MyPost",
-            "userRef":2,
-            "timestamp":3,
-            "tags":3,
-            "image":3,
-            "description":3,
-            "likes":3
-        },{
-            "id":"YourPost",
-            "userRef":2,
-            "timestamp":3,
-            "tags":3,
-            "image":3,
-            "description":3,
-            "likes":3
-        }]
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
+function SocialSearchBar({ posts }) {
+	const [searchTerm, setSearchTerm] = useState("");
 
-    return(
-    <div className="SearchBar">
-        <input className="form-control" type="text" placeholder=" Search for a post..." onChange={(event)=>{
-            setSearchTerm(event.target.value);
-        }}/>   
-        
-        {dummyData.filter((val)=>{
-            
-            if(searchTerm===""){
-                return ""
-            }else if(val.id.toLowerCase().includes(searchTerm.toLowerCase())){
-                return val
-            }
-            return null
-
-        }).map((val,key)=>{
-            return(
-                <div className="ResultsItem">
-                <p>{val.id}</p>
-                </div>
-            );
-        })}
-    </div>
-    );
+	return (
+		<div className="w-100 mt-3 searchBar position-relative">
+			<input
+				className="form-control"
+				type="text"
+				placeholder=" Search for a post..."
+				value={searchTerm}
+				onChange={(event) => {
+					setSearchTerm(event.target.value);
+				}}
+			/>
+			{searchTerm ? (
+				<AiFillCloseCircle size={25} className="searchIcon d-flex align-items-center position-absolute" onClick={() => setSearchTerm("")} />
+			) : (
+				<FaSearch size={25} className="searchIcon d-flex align-items-center position-absolute" />
+			)}
+			<div className="resultsContainer mt-2 rounded">
+				{posts
+					.filter((val) => {
+						if (searchTerm === "") {
+							return "";
+						} else if (val.description.toLowerCase().includes(searchTerm.toLowerCase()) || val.tags.toString().toLowerCase().includes(searchTerm.toLowerCase())) {
+							return val;
+						}
+						return null;
+					})
+					.map((val, key) => {
+						return (
+							<div key={key} className="resultItem d-flex align-content-center align-items-center">
+								<img width="40px" className="resultImage m-1 rounded-1" src={val?.image} alt={val?.description} />
+								<p>{val?.description}</p>
+							</div>
+						);
+					})}
+			</div>
+		</div>
+	);
 }
 export default SocialSearchBar;
