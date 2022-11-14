4ed04fd0366e7cbd6f9d497ea08803fc3537e066
tiago <tiagoribeiro64625@gmail.com>
2022-11-13 23:04:28 -0500

    initial changes

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Image, Container, Row, Button, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { doc, collection, getDoc, setDoc, getDocs, deleteDoc } from "firebase/firestore";
+import { doc, collection, getDoc, setDoc, getDocs, deleteDoc, onSnapshot, query, orderBy, limit } from "firebase/firestore";
+// Add where up here leaving out temporarily so we don't get a warning
+// Make relevant edits to line 65
 import { useAuth } from "../contexts/AuthContext";
+import PostFeedItem from "../components/PostFeedItem";
 import { db } from "../firebase";
 
 import "./UserProfilePage.css";
@@ -11,6 +14,7 @@ function UserProfilePage() {
 	const { currentUser } = useAuth();
 
 	const [user, setUser] = useState();
+	const [posts, setPosts] = useState([]);
 	const [followersCount, setFollowersCount] = useState(0);
 	const [followingCount, setFollowingCount] = useState(0);
 	const [isFollowing, setIsFollowing] = useState(null);
@@ -56,6 +60,13 @@ function UserProfilePage() {
 		checkFollowing();
 	}, [currentUser.uid, userid]);
 
+	useEffect(() => {
+		// where('userid', '==', userid),  < add this line into the query
+		onSnapshot(query(collection(db, `posts`), orderBy("timestamp", "desc"), limit(5)), (snapshot) => {
+			setPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		});
+	}, [currentUser]); // < Throw in parameter we need here
+
 	async function followUser() {
 		await setDoc(doc(db, `users/${currentUser.uid}/following`, user.id), {});
 		await setDoc(doc(db, `users/${user.id}/followers`, currentUser.uid), {});
@@ -71,18 +82,57 @@ function UserProfilePage() {
 	}
 
 	return (
-		<Container fluid className="mainPage" style={{ color: "white" }}>
-			<Col>
-				<Row className="noMargin">Name: {user?.name}</Row>
-				<Row className="noMargin">Email: {user?.email}</Row>
-				<Row className="noMargin">ID: {user?.uid}</Row>
-				<Image roundedCircle height="100px" src={user?.photoURL} />
-
-				{currentUser?.uid !== userid && (isFollowing ? <Button onClick={() => unfollowUser()}>Unfollow</Button> : <Button onClick={() => followUser()}>Follow</Button>)}
-
-				<h4>Followers: {followersCount}</h4>
-				<h4>Following: {followingCount}</h4>
-			</Col>
+		<Container className="mainPage" style={{ color: "white" }}>
+			<br />
+			<div className="card bg-dark text-white">
+			<Container>
+				<Row>
+						{/* This column holds the profile image */}
+						<Col className="m-4 col-md-2"> 
+							<Image roundedCircle height="200px" src={user?.photoURL} />
+						</Col>
+						<Col className="m-4 mt-9 col-md-5">
+							<br />
+							<h2><Row className="noMargin fs-2 fw-bold">{user?.name}</Row></h2>
+							
+							<p><Row className="noMargin">Email: {user?.email}  ID: {user?.uid}</Row></p>
+							
+						</Col>
+						<Col>
+							<Row>
+							{currentUser?.uid !== userid && (isFollowing ? <Button onClick={() => unfollowUser()}>Unfollow</Button> : <Button onClick={() => followUser()}>Follow</Button>)}
+							<Button onClick={() => unfollowUser()}>Unfollow</Button>
+							</Row>
+							<Row className="d-flex align-items-center justify-content-between">
+								<Col className="col-md-4">
+									<div>
+										<h4>Followers:</h4>
+										{followersCount}
+									</div>
+								</Col>
+								<Col className="col-md-4">
+									<div>
+										<h4>Following:</h4>
+										{followingCount}
+									</div>
+								</Col>
+							</Row>
+						</Col>
+				</Row>
+			</Container>
+			</div>
+			<br />
+				<Container className="d-flex align-items-center justify-content-between" style={{width : "69%"}}>
+					<Col>
+						{posts?.length > 0 ? (
+							posts.map(({ id, userRef, tags, timestamp, image, description, likes }) => (
+								<PostFeedItem id={id} userRef={userRef} tags={tags} image={image} timestamp={timestamp} description={description} likes={likes} key={id} />
+							))
+						) : (
+							<h1 className="text-center mt-3">No posts yet</h1>
+						)}
+					</Col>
+				</Container>
 		</Container>
 	);
 }
