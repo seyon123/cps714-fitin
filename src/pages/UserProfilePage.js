332ed8a320f709d341dd4695fba01b85a7a9da4a
Raj Mistry <56315897+raj-mistry@users.noreply.github.com>
2022-11-21 19:28:19 -0500

    verification badge

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -151,6 +151,8 @@ function UserProfilePage() {
                                         <h2>
                                             <Row className="noMargin fs-2 fw-bold">
                                                 {user?.name}
+                                                
+                                                <img style={{marginTop: "-30px", height: "100px", width: "auto", mixBlendMode: "lighten", filter: "hue-rotate(40deg) "}}src="/verification.gif"/>
                                             </Row>
                                         </h2>
                                         <p className="text-white-50">
